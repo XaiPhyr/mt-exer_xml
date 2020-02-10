0c6a6168e2d2059ea0ad480ad1aa5e88e1d5532e
xaiphyr <arcadia.initiative@gmail.com>
2020-02-10 09:36:44 +0800

    removed file, changed layout

--- views/rss.php
+++ views/rss.php
@@ -5,10 +5,18 @@ $domOBJ->load("https://mt-exe-1.herokuapp.com/");
 $content = $domOBJ->getElementsByTagName("item");
 
 foreach ($content as $data) {
+
     $id = $data->getElementsByTagName("Id")->item(0)->nodeValue;
     $name = $data->getElementsByTagName("Name")->item(0)->nodeValue;
     $address = $data->getElementsByTagName("Address")->item(0)->nodeValue;
     $contacts = $data->getElementsByTagName("Contacts")->item(0)->nodeValue;
 
-    echo "$id :: $name - $address <br>";
+    echo "<ul>
+    <li><strong>$id</strong></li>
+    <ul>
+    <li>Name: <strong>$name</strong></li>
+    <li>Address: <strong>$address</strong></li>
+    <li>Contacts: <strong>$contacts</strong></li>
+    </ul>
+    </ul>";
 }
